31f2741ceca3fa25efe9f62969e13fee1d457fa8
contrepoint <contrepoint21@gmail.com>
2016-03-21 15:11:55 +0800

    finish 3.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+  def setup
+   @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
 RSpec.feature "static page content", :type => :feature do
   scenario "should have the content 'Sample App'" do
@@ -8,8 +11,8 @@ RSpec.feature "static page content", :type => :feature do
   end
 
   scenario "should have the title 'Home'" do
-    visit '/static_pages/home'
-    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    visit static_pages_home_path
+    expect(page).to have_title("#{@base_title} | Home")
   end
 
   scenario "should have the content 'Help'" do
@@ -19,7 +22,7 @@ RSpec.feature "static page content", :type => :feature do
 
   scenario "should have the title 'Help'" do
     visit '/static_pages/help'
-    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    expect(page).to have_title("#{@base_title} | Help")
   end
 
   scenario "should have the content 'About Us'" do
@@ -29,6 +32,16 @@ RSpec.feature "static page content", :type => :feature do
 
   scenario "should have the title 'About Us'" do
     visit '/static_pages/about'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    expect(page).to have_title("#{@base_title} | About Us")
+  end
+
+  scenario "should have the content 'Contact'" do
+  	visit '/static_pages/contact'
+  	expect(page).to have_text('Contact')
+  end
+
+  scenario "should have the title 'Contact'" do
+    visit '/static_pages/contact'
+    expect(page).to have_title("#{@base_title} | Contact")
   end
 end
