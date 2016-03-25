8cbf9a0df6e17605a7c68ffa4ea14cff00174f47
contrepoint <contrepoint21@gmail.com>
2016-03-25 17:51:17 +0800

    finish chapter 7.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,6 +6,8 @@ require 'rails_helper'
 
 RSpec.feature "User Sign Up", type: :feature do
 	background { visit signup_path }
+	let(:submit) { "Create my account" }
+
 	scenario "should have content 'Sign up'" do
 		expect(page).to have_text('Sign up')
 	end
@@ -14,6 +16,17 @@ RSpec.feature "User Sign Up", type: :feature do
 		expect(page).to have_title("#{@base_title} | Sign up")
 	end
 
+	scenario 'when signup is invalid, user is not created' do
+    expect { click_button submit }.not_to change(User, :count)
+  end
+
+	scenario 'when signup is valid, user is created' do
+    fill_in "Name",         with: "John Doe"
+    fill_in "Email",        with: "john@example.com"
+    fill_in "Password",     with: "123456"
+    fill_in "Confirmation", with: "123456"
+    expect { click_button submit }.to change(User, :count).by(1)
+  end
 end
 
 RSpec.feature "User Profile Page", type: :feature do
